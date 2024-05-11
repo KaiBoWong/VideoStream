2f8effaf05e3cc4f90a7ff7d3d6c7ddf33cac2f1
KaiBoWong <153863639+KaiBoWong@users.noreply.github.com>
2024-05-11 13:46:01 +0800

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,34 +20,24 @@
             border-color: #450A0A;
             color: white;
         }
-
         /* Your existing custom CSS styles */
         /* Additional styles for the card header */
         .card-header {
-            font-size: 20px;
-            background-color: #450A0A;
-            /* Set background color */
-            color: white;
-            /* Set text color */
-            border-bottom: none;
-            /* Remove bottom border */
-            border-radius: 0;
-            /* Remove border radius */
-            font-weight: bold;
-            /* Set font weight */
-            text-align: center;
-            /* Center align text */
-            padding: 20px;
-            /* Add padding */
+            background-color: #450A0A; /* Set background color */
+            color: white; /* Set text color */
+            border-bottom: none; /* Remove bottom border */
+            border-radius: 0; /* Remove border radius */
+            font-weight: bold; /* Set font weight */
+            text-align: center; /* Center align text */
+            padding: 20px; /* Add padding */
         }
 
         .card-body {
-            margin-top: 40px;
-            /* Add margin top */
+            margin-top: 20px; /* Add margin top */
         }
     </style>
     <div class="container" style="height: 80vh;overflow: auto;">
-        <div class="row justify-content-center" style="display:flex;justify-content:center;align-items:center;">
+        <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card"
                     style="background-color:rgba(0, 0, 0, .5); border-color: #450A0A;box-shadow: 0 0 30px #450A0A;">
@@ -57,8 +47,7 @@
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
 
-                            <div class="row mb-3"
-                                style="color:white;margin-bottom:20px;display:flex;justify-content:center;align-items:center;">
+                            <div class="row mb-3" style="color:white;">
                                 <label for="username"
                                     class="col-md-4 col-form-label text-md-end">{{ __('Username') }}</label>
 
@@ -67,21 +56,14 @@
                                         class="form-control @error('username') is-invalid @enderror" name="username"
                                         value="{{ old('username') }}" required autocomplete="username" autofocus>
 
-                                    <!-- New row for the error message -->
-                                    <div class="row mt-1">
-                                        <div style="margin-top:10px;padding-left:15px;">
-                                            @error('username')
-                                                <span class="invalid-feedback" role="alert"
-                                                    style="display: block; font-size: 0.875rem; color: red;">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
+                                    @error('username')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                 </div>
                             </div>
-                            <div class="row mb-3"
-                                style="color:white;margin-bottom:20px;display:flex;justify-content:center;align-items:center;">
+                            <div class="row mb-3" style="color:white;">
                                 <label for="email"
                                     class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
@@ -89,108 +71,104 @@
                                     <input id="email" type="email"
                                         class="form-control @error('email') is-invalid @enderror" name="email"
                                         value="{{ old('email') }}" required autocomplete="email">
-                                    <!-- New row for the error message -->
-                                    <div class="row mt-1">
-                                        <div style="margin-top:10px;padding-left:15px;">
-                                            @error('email')
-                                                <span class="invalid-feedback" role="alert"
-                                                    style="display: block; font-size: 0.875rem; color: red;">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
-                                    </div>
+
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
                                 </div>
                             </div>
 
-                            <div class="row mb-3"
-                                style="color:white;margin-bottom:20px;display:flex;justify-content:center;align-items:center;">
+                            <div class="row mb-3" style="color:white;">
                                 <label for="password"
                                     class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
 
                                 <div class="col-md-6">
-                                    <div class="input-group" style="display:flex;justify-content:row;">
+                                    <div class="input-group">
                                         <input id="password" type="password"
                                             class="form-control @error('password') is-invalid @enderror" name="password"
                                             required autocomplete="current-password">
 
                                         <button type="button" class="btn btn-outline-secondary" id="togglePassword"><i
                                                 class="fas fa-eye"></i></button>
-                                    </div>
 
-                                    <!-- New row for the error message -->
-                                    <div class="row mt-1">
-                                        <div style="margin-top:10px;padding-left:15px;">
-                                            @error('password')
-                                                <span class="invalid-feedback" role="alert"
-                                                    style="display: block; font-size: 0.875rem; color: red;">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
                                     </div>
                                 </div>
+                                <script>
+                                    $(document).ready(function() {
+                                        $('#togglePassword').click(function() {
+                                            var passwordField = $('#password');
+                                            var passwordFieldType = passwordField.attr('type');
+
+                                            if (passwordFieldType === 'password') {
+                                                passwordField.attr('type', 'text');
+                                                $(this).html('<i class="fa fa-eye-slash"></i>');
+                                            } else {
+                                                passwordField.attr('type', 'password');
+                                                $(this).html('<i class="fa fa-eye"></i>');
+                                            }
+                                        });
+                                    });
+                                </script>
                             </div>
 
-                            <div class="row mb-3"
-                                style="color:white;margin-bottom:40px;display:flex;justify-content:center;align-items:center;">
+                            <div class="row mb-3" style="color:white;">
                                 <label for="password-confirm"
                                     class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
 
                                 <div class="col-md-6">
-                                    <div class="input-group" style="display:flex;justify-content:row;">
-                                        <input id="password-confirm" type="password"
-                                            class="form-control @error('password-confirm') is-invalid @enderror"
+                                    <div class="input-group">
+                                        <input id="password-confirm" type="password" class="form-control @error('password-confirm') is-invalid @enderror"
                                             name="password_confirmation" required autocomplete="new-password">
 
                                         <button type="button" class="btn btn-outline-secondary" id="togglePassword1"><i
                                                 class="fas fa-eye"></i></button>
+
+                                        @error('password-confirm')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
                                     </div>
-                                    <!-- New row for the error message -->
-                                    <div class="row mt-1">
-                                        <div style="margin-top:10px;padding-left:15px;">
-                                            @error('password-confirm')
-                                                <span class="invalid-feedback" role="alert"
-                                                    style="display: block; font-size: 0.875rem; color: red;">
-                                                    <strong>{{ $message }}</strong>
-                                                </span>
-                                            @enderror
-                                        </div>
+                                </div>
+                                <script>
+                                    $(document).ready(function() {
+                                        $('#togglePassword1').click(function() {
+                                            var passwordField = $('#password-confirm');
+                                            var passwordFieldType = passwordField.attr('type');
+
+                                            if (passwordFieldType === 'password') {
+                                                passwordField.attr('type', 'text');
+                                                $(this).html('<i class="fa fa-eye-slash"></i>');
+                                            } else {
+                                                passwordField.attr('type', 'password');
+                                                $(this).html('<i class="fa fa-eye"></i>');
+                                            }
+                                        });
+                                    });
+                                </script>
+                            </div>
+                            <div class="row mb-4 mt-4">
+                                <div class="col-md-6 offset-md-4"
+                                    style="display:flex;justify-content:center;align-items:center;">
+                                    <button type="submit" class="btn btn-primary" style="width:150px;">
+                                        {{ __('Register') }}
+                                    </button>
+                                    <div style="padding-left:30px;">
+                                        <a href="{{ route('home') }}" class="btn">{{ __('Back') }}</a>
                                     </div>
                                 </div>
                             </div>
-                            <script>
-                                $(document).ready(function() {
-                                    $('#togglePassword1').click(function() {
-                                        var passwordField = $('#password-confirm');
-                                        var passwordFieldType = passwordField.attr('type');
-
-                                        if (passwordFieldType === 'password') {
-                                            passwordField.attr('type', 'text');
-                                            $(this).html('<i class="fa fa-eye-slash"></i>');
-                                        } else {
-                                            passwordField.attr('type', 'password');
-                                            $(this).html('<i class="fa fa-eye"></i>');
-                                        }
-                                    });
-                                });
-                            </script>
+                        </form>
                     </div>
-                    <div class="row mb-4 mt-4"
-                        style="margin-bottom:20px;display:flex;justify-content:center;align-items:center;">
-                        <div class="col-md-6 offset-md-4" style="display:flex;justify-content:center;align-items:center;">
-                            <button type="submit" class="btn btn-primary" style="width:150px;">
-                                {{ __('Register') }}
-                            </button>
-                            <div style="padding-left:30px;">
-                                <a href="{{ route('home') }}" class="btn">{{ __('Back') }}</a>
-                            </div>
-                        </div>
-                    </div>
-                    </form>
                 </div>
             </div>
         </div>
-    </div>
     </div>
 @endsection
